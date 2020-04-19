37d820de0aef81a40f44a885b8af5d770f3a7267
Aaron <rustinante@gmail.com>
2020-04-18 19:40:31 -0700

    percentile_by in stats.rs

--- src/stats.rs
+++ src/stats.rs
@@ -1,3 +1,4 @@
+use std::cmp::{min, Ordering};
 use std::ops::Deref;
 
 use num::traits::ToPrimitive;
@@ -10,8 +11,11 @@ pub fn n_choose_2(n: usize) -> usize {
     }
 }
 
-pub fn kahan_sigma<'a, E, I: Iterator<Item=&'a E>, F>(element_iterator: I, op: F) -> f64
-    where E: Copy + 'a, &'a E: Deref, F: Fn(E) -> f64 {
+pub fn kahan_sigma<'a, E, I: Iterator<Item = &'a E>, F>(element_iterator: I, op: F) -> f64
+where
+    E: Copy + 'a,
+    &'a E: Deref,
+    F: Fn(E) -> f64 {
     // Kahan summation algorithm
     let mut sum = 0f64;
     let mut lower_bits = 0f64;
@@ -24,8 +28,11 @@ pub fn kahan_sigma<'a, E, I: Iterator<Item=&'a E>, F>(element_iterator: I, op: F
     sum
 }
 
-pub fn kahan_sigma_f32<'a, E, I: Iterator<Item=&'a E>, F>(element_iterator: I, op: F) -> f32
-    where E: Copy + 'a, &'a E: Deref, F: Fn(E) -> f32 {
+pub fn kahan_sigma_f32<'a, E, I: Iterator<Item = &'a E>, F>(element_iterator: I, op: F) -> f32
+where
+    E: Copy + 'a,
+    &'a E: Deref,
+    F: Fn(E) -> f32 {
     // Kahan summation algorithm
     let mut sum = 0f32;
     let mut lower_bits = 0f32;
@@ -38,11 +45,14 @@ pub fn kahan_sigma_f32<'a, E, I: Iterator<Item=&'a E>, F>(element_iterator: I, o
     sum
 }
 
-pub fn kahan_sigma_return_counter<'a, E, I: Iterator<Item=&'a E>, F>(
+pub fn kahan_sigma_return_counter<'a, E, I: Iterator<Item = &'a E>, F>(
     element_iterator: I,
-    op: F,
+    op: F
 ) -> (f64, usize)
-    where E: Copy + 'a, &'a E: Deref, F: Fn(E) -> f64 {
+where
+    E: Copy + 'a,
+    &'a E: Deref,
+    F: Fn(E) -> f64 {
     let mut count = 0usize;
     // Kahan summation algorithm
     let mut sum = 0f64;
@@ -58,20 +68,26 @@ pub fn kahan_sigma_return_counter<'a, E, I: Iterator<Item=&'a E>, F>(
 }
 
 #[inline]
-pub fn sum<'a, A, T: Iterator<Item=&'a A>>(element_iterator: T) -> f64
-    where A: Copy + ToPrimitive + 'a, &'a A: Deref {
+pub fn sum<'a, A, T: Iterator<Item = &'a A>>(element_iterator: T) -> f64
+where
+    A: Copy + ToPrimitive + 'a,
+    &'a A: Deref {
     kahan_sigma(element_iterator, |a| a.to_f64().unwrap())
 }
 
 #[inline]
-pub fn sum_f32<'a, A, T: Iterator<Item=&'a A>>(element_iterator: T) -> f32
-    where A: Copy + ToPrimitive + 'a, &'a A: Deref {
+pub fn sum_f32<'a, A, T: Iterator<Item = &'a A>>(element_iterator: T) -> f32
+where
+    A: Copy + ToPrimitive + 'a,
+    &'a A: Deref {
     kahan_sigma_f32(element_iterator, |a| a.to_f32().unwrap())
 }
 
 #[inline]
-pub fn sum_of_squares<'a, A, T: Iterator<Item=&'a A>>(element_iterator: T) -> f64
-    where A: Copy + ToPrimitive + 'a, &'a A: Deref {
+pub fn sum_of_squares<'a, A, T: Iterator<Item = &'a A>>(element_iterator: T) -> f64
+where
+    A: Copy + ToPrimitive + 'a,
+    &'a A: Deref {
     kahan_sigma(element_iterator, |a| {
         let a_f64 = a.to_f64().unwrap();
         a_f64 * a_f64
@@ -79,8 +95,10 @@ pub fn sum_of_squares<'a, A, T: Iterator<Item=&'a A>>(element_iterator: T) -> f6
 }
 
 #[inline]
-pub fn sum_of_squares_f32<'a, A, T: Iterator<Item=&'a A>>(element_iterator: T) -> f32
-    where A: Copy + ToPrimitive + 'a, &'a A: Deref {
+pub fn sum_of_squares_f32<'a, A, T: Iterator<Item = &'a A>>(element_iterator: T) -> f32
+where
+    A: Copy + ToPrimitive + 'a,
+    &'a A: Deref {
     kahan_sigma_f32(element_iterator, |a| {
         let a_f32 = a.to_f32().unwrap();
         a_f32 * a_f32
@@ -88,8 +106,10 @@ pub fn sum_of_squares_f32<'a, A, T: Iterator<Item=&'a A>>(element_iterator: T) -
 }
 
 #[inline]
-pub fn sum_of_fourth_power_f32<'a, A, T: Iterator<Item=&'a A>>(element_iterator: T) -> f32
-    where A: Copy + ToPrimitive + 'a, &'a A: Deref {
+pub fn sum_of_fourth_power_f32<'a, A, T: Iterator<Item = &'a A>>(element_iterator: T) -> f32
+where
+    A: Copy + ToPrimitive + 'a,
+    &'a A: Deref {
     kahan_sigma_f32(element_iterator, |a| {
         let a_f32 = a.to_f32().unwrap();
         a_f32 * a_f32 * a_f32 * a_f32
@@ -97,8 +117,10 @@ pub fn sum_of_fourth_power_f32<'a, A, T: Iterator<Item=&'a A>>(element_iterator:
 }
 
 #[inline]
-pub fn mean<'a, A, T: Iterator<Item=&'a A>>(element_iterator: T) -> f64
-    where A: Copy + ToPrimitive + 'a, &'a A: Deref {
+pub fn mean<'a, A, T: Iterator<Item = &'a A>>(element_iterator: T) -> f64
+where
+    A: Copy + ToPrimitive + 'a,
+    &'a A: Deref {
     let (sum, count) = kahan_sigma_return_counter(element_iterator, |a| a.to_f64().unwrap());
     sum / count as f64
 }
@@ -108,8 +130,10 @@ pub fn mean<'a, A, T: Iterator<Item=&'a A>>(element_iterator: T) -> f64
 /// for population variance, set `ddof` to 0
 /// for sample variance, set `ddof` to 1
 #[inline]
-pub fn variance<'a, T: Clone + Iterator<Item=&'a A>, A>(element_iterator: T, ddof: usize) -> f64
-    where A: Copy + ToPrimitive + 'a, &'a A: Deref {
+pub fn variance<'a, T: Clone + Iterator<Item = &'a A>, A>(element_iterator: T, ddof: usize) -> f64
+where
+    A: Copy + ToPrimitive + 'a,
+    &'a A: Deref {
     let mean = mean(element_iterator.clone());
     let (sum, count) = kahan_sigma_return_counter(element_iterator, move |a| {
         let a_f64 = a.to_f64().unwrap() - mean;
@@ -123,19 +147,47 @@ pub fn variance<'a, T: Clone + Iterator<Item=&'a A>, A>(element_iterator: T, ddo
 /// for population standard deviation, set `ddof` to 0
 /// for sample standard deviation, set `ddof` to 1
 #[inline]
-pub fn standard_deviation<'a, T: Clone + Iterator<Item=&'a A>, A>(
+pub fn standard_deviation<'a, T: Clone + Iterator<Item = &'a A>, A>(
     element_iterator: T,
-    ddof: usize,
+    ddof: usize
 ) -> f64
-    where A: Copy + ToPrimitive + 'a, &'a A: Deref {
+where
+    A: Copy + ToPrimitive + 'a,
+    &'a A: Deref {
     variance(element_iterator, ddof).sqrt()
+}
+
+/// `percentile_ratio` is percentile / 100
+pub fn percentile_by<T, F>(
+    mut numbers: Vec<T>,
+    percentile_ratio: f64,
+    mut compare: F
+) -> Result<T, String>
+where
+    T: Clone,
+    F: FnMut(&T, &T) -> Ordering {
+    if numbers.len() == 0 || percentile_ratio < 0. || percentile_ratio > 1. {
+        return Err("percentile_by received an empty vector".to_string());
+    }
+    numbers.sort_by(|a, b| compare(a, b));
+
+    Ok(numbers[min(
+        (numbers.len() as f64 * percentile_ratio).floor() as usize,
+        numbers.len() - 1
+    )]
+    .clone())
 }
 
 #[cfg(test)]
 mod tests {
+    use std::iter::{FromIterator, Iterator};
+
+    use rand::seq::SliceRandom;
     use rand::Rng;
 
-    use super::{mean, standard_deviation, sum, sum_of_squares, variance};
+    use super::{mean, percentile_by, standard_deviation, sum, sum_of_squares, variance};
+
+    const F64_ERROR_TOLERANCE: f64 = 1e-6;
 
     #[test]
     fn test_sum() {
@@ -156,7 +208,10 @@ mod tests {
         for _ in 0..1000000 {
             numbers.push(rng.gen_range(1, 21));
         }
-        assert_eq!(numbers.iter().sum::<i64>() as f64 / numbers.len() as f64, mean(numbers.iter()));
+        assert_eq!(
+            numbers.iter().sum::<i64>() as f64 / numbers.len() as f64,
+            mean(numbers.iter())
+        );
     }
 
     #[test]
@@ -171,5 +226,54 @@ mod tests {
         let elements = vec![1, 5, 3, 2, 7, 100, 1234, 234, 12, 0, 1234];
         assert_eq!(487.9947466185192, standard_deviation(elements.iter(), 1));
         assert_eq!(465.28473464914003, standard_deviation(elements.iter(), 0));
+    }
+
+    #[test]
+    fn test_percentile_by() {
+        let mut rng = rand::thread_rng();
+        {
+            let mut v1 = vec![-0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7];
+            for _ in 0..5 {
+                v1.shuffle(&mut rng);
+                for i in (0..100).step_by(5) {
+                    assert!(
+                        ((i / 10) as f64 / 10.
+                            - 0.2
+                            - percentile_by(v1.clone(), i as f64 / 100., |a, b| {
+                                a.partial_cmp(b).unwrap()
+                            })
+                            .unwrap())
+                        .abs()
+                            < F64_ERROR_TOLERANCE
+                    );
+                }
+            }
+        }
+
+        {
+            let mut v2 = vec![-2, -1, 0, 1, 2, 3, 4, 5, 6, 7];
+            for _ in 0..5 {
+                v2.shuffle(&mut rng);
+                for i in (0..100).step_by(5) {
+                    assert_eq!(
+                        (i / 10 - 2),
+                        percentile_by(v2.clone(), i as f64 / 100., |a, b| { a.cmp(b) }).unwrap()
+                    );
+                }
+            }
+        }
+
+        {
+            let mut v3 = Vec::from_iter(-100..900);
+            for _ in 0..5 {
+                v3.shuffle(&mut rng);
+                for i in 0..100 {
+                    assert_eq!(
+                        (i - 10) * 10,
+                        percentile_by(v3.clone(), i as f64 / 100., |a, b| { a.cmp(b) }).unwrap()
+                    )
+                }
+            }
+        }
     }
 }
