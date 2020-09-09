e825f2d80d00ce297cedbb18f3c762cdf969923c
Aaron Zorel <rustinante@gmail.com>
2020-09-08 22:03:45 -0700

    pub use AsUnionZipped

--- src/iter.rs
+++ src/iter.rs
@@ -12,4 +12,6 @@ pub use binned_interval_iter::{
 pub use common_refinement_zip::{CommonRefinementZip, CommonRefinementZipped};
 pub use concatenated_iter::{ConcatenatedIter, IntoConcatIter};
 pub use flat_zip::{FlatZipIter, IntoFlatZipIter};
-pub use union_zip::{IntoUnionZip, UnionZip, UnionZipped, UnionZippedIter};
+pub use union_zip::{
+    AsUnionZipped, IntoUnionZip, UnionZip, UnionZipped, UnionZippedIter,
+};
