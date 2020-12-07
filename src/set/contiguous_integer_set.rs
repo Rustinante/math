4263121c20c150d8fecd4c921b48ccd7cea7fff9
Aaron Zorel <rustinante@gmail.com>
2020-12-06 23:22:20 -0800

    update unit tests for is_subset_of and is_strict_subset_of

--- src/set/contiguous_integer_set.rs
+++ src/set/contiguous_integer_set.rs
@@ -35,11 +35,12 @@ impl<E: Integer + Copy> ContiguousIntegerSet<E> {
     }
 
     pub fn is_subset_of(&self, other: &ContiguousIntegerSet<E>) -> bool {
-        self.start >= other.start && self.end <= other.end
+        // note that the empty set is a subset of any set
+        self.is_empty() || (other.start <= self.start && self.end <= other.end)
     }
 
     pub fn is_strict_subset_of(&self, other: &ContiguousIntegerSet<E>) -> bool {
-        self.is_subset_of(&other) && self != other
+        self.is_subset_of(&other) && (self != other) && !other.is_empty()
     }
 
     #[inline]
@@ -306,36 +307,32 @@ mod tests {
 
     #[test]
     fn test_ord() {
-        assert!(
-            ContiguousIntegerSet::new(2, 5) < ContiguousIntegerSet::new(3, 4)
-        );
-        assert!(
-            ContiguousIntegerSet::new(2, 5) < ContiguousIntegerSet::new(3, 5)
-        );
-        assert!(
-            ContiguousIntegerSet::new(2, 5) < ContiguousIntegerSet::new(3, 8)
-        );
-        assert!(
-            ContiguousIntegerSet::new(2, 3) < ContiguousIntegerSet::new(4, 5)
-        );
-        assert!(
-            ContiguousIntegerSet::new(2, 5) > ContiguousIntegerSet::new(2, 4)
-        );
-        assert!(
-            ContiguousIntegerSet::new(2, 5) < ContiguousIntegerSet::new(2, 8)
-        );
+        macro_rules! expect_le {
+            ($a:expr, $b:expr, $c:expr, $d:expr) => {
+                let s1 = ContiguousIntegerSet::new($a, $b);
+                let s2 = ContiguousIntegerSet::new($c, $d);
+                assert!(s1 < s2);
+            };
+        }
+        macro_rules! expect_ge {
+            ($a:expr, $b:expr, $c:expr, $d:expr) => {
+                let s1 = ContiguousIntegerSet::new($a, $b);
+                let s2 = ContiguousIntegerSet::new($c, $d);
+                assert!(s1 > s2);
+            };
+        }
+        expect_le!(2, 5, 3, 4);
+        expect_le!(2, 5, 3, 5);
+        expect_le!(2, 5, 3, 8);
+        expect_le!(2, 3, 4, 5);
+        expect_le!(2, 5, 2, 8);
+        expect_ge!(2, 5, 2, 4);
+        expect_ge!(2, 5, 1, 8);
+        expect_ge!(2, 5, 1, 5);
+        expect_ge!(2, 5, 1, 3);
         assert_eq!(
             ContiguousIntegerSet::new(2, 2),
             ContiguousIntegerSet::new(2, 2)
-        );
-        assert!(
-            ContiguousIntegerSet::new(2, 5) > ContiguousIntegerSet::new(1, 8)
-        );
-        assert!(
-            ContiguousIntegerSet::new(2, 5) > ContiguousIntegerSet::new(1, 5)
-        );
-        assert!(
-            ContiguousIntegerSet::new(2, 5) > ContiguousIntegerSet::new(1, 3)
         );
     }
 
@@ -354,16 +351,367 @@ mod tests {
     }
 
     #[test]
-    fn test_strict_subset() {
-        let s1 = ContiguousIntegerSet::new(2, 4);
-        let s2 = ContiguousIntegerSet::new(2, 5);
+    fn test_is_subset_of() {
+        macro_rules! ab_is_subset_of_cd {
+            (
+                $a:expr,
+                $b:expr,
+                $c:expr,
+                $d:expr,
+                $is_subset:expr,
+                $is_strict_subset:expr
+            ) => {
+                let s1 = ContiguousIntegerSet::new($a as i32, $b);
+                let s2 = ContiguousIntegerSet::new($c as i32, $d);
+                assert_eq!(s1.is_subset_of(&s2), $is_subset);
+                assert_eq!(s1.is_strict_subset_of(&s2), $is_strict_subset);
 
-        assert!(s1.is_subset_of(&s2));
-        assert!(s1.is_strict_subset_of(&s2));
+                let s1 = ContiguousIntegerSet::new($a as i64, $b);
+                let s2 = ContiguousIntegerSet::new($c as i64, $d);
+                assert_eq!(s1.is_subset_of(&s2), $is_subset);
+                assert_eq!(s1.is_strict_subset_of(&s2), $is_strict_subset);
+            };
+        }
 
-        let s3 = ContiguousIntegerSet::new(2, 5);
+        macro_rules! test_nonnegative_abcd {
+            (
+                $a:expr,
+                $b:expr,
+                $c:expr,
+                $d:expr,
+                $is_subset:expr,
+                $is_strict_subset:expr
+            ) => {
+                // test the signed type
+                ab_is_subset_of_cd!(
+                    $a,
+                    $b,
+                    $c,
+                    $d,
+                    $is_subset,
+                    $is_strict_subset
+                );
 
-        assert!(s2.is_subset_of(&s3));
-        assert!(!s2.is_strict_subset_of(&s3));
+                let s1 = ContiguousIntegerSet::new($a as u32, $b);
+                let s2 = ContiguousIntegerSet::new($c as u32, $d);
+                assert_eq!(s1.is_subset_of(&s2), $is_subset);
+                assert_eq!(s1.is_strict_subset_of(&s2), $is_strict_subset);
+
+                let s1 = ContiguousIntegerSet::new($a as u64, $b);
+                let s2 = ContiguousIntegerSet::new($c as u64, $d);
+                assert_eq!(s1.is_subset_of(&s2), $is_subset);
+                assert_eq!(s1.is_strict_subset_of(&s2), $is_strict_subset);
+            };
+        }
+
+        // signs
+        // - - - -
+        ab_is_subset_of_cd!(-10, -5, -20, -25, false, false);
+        ab_is_subset_of_cd!(-10, -5, -25, -20, false, false);
+        ab_is_subset_of_cd!(-10, -5, -25, -10, false, false);
+        ab_is_subset_of_cd!(-10, -5, -25, -7, false, false);
+        ab_is_subset_of_cd!(-10, -5, -10, -10, false, false);
+        ab_is_subset_of_cd!(-10, -5, -10, -7, false, false);
+        ab_is_subset_of_cd!(-10, -5, -8, -7, false, false);
+        ab_is_subset_of_cd!(-10, -5, -8, -5, false, false);
+        ab_is_subset_of_cd!(-10, -5, -8, -1, false, false);
+        ab_is_subset_of_cd!(-10, -5, -5, -1, false, false);
+        ab_is_subset_of_cd!(-10, -5, -2, -1, false, false);
+
+        ab_is_subset_of_cd!(-10, -5, -25, -5, true, true);
+        ab_is_subset_of_cd!(-10, -5, -25, -1, true, true);
+        ab_is_subset_of_cd!(-10, -5, -10, -5, true, false);
+        ab_is_subset_of_cd!(-10, -5, -10, -2, true, true);
+
+        // the empty set is a subset of any set
+        ab_is_subset_of_cd!(-5, -10, -20, -25, true, false);
+        ab_is_subset_of_cd!(-5, -10, -25, -20, true, true);
+        ab_is_subset_of_cd!(-5, -10, -25, -2, true, true);
+        ab_is_subset_of_cd!(-5, -10, -10, -2, true, true);
+        ab_is_subset_of_cd!(-5, -10, -2, -1, true, true);
+
+        // signs
+        // - - - +
+        ab_is_subset_of_cd!(-15, -5, -10, 0, false, false);
+        ab_is_subset_of_cd!(-15, -5, -5, 0, false, false);
+        ab_is_subset_of_cd!(-15, -5, -2, 0, false, false);
+        ab_is_subset_of_cd!(-15, -5, 0, 0, false, false);
+
+        ab_is_subset_of_cd!(-15, -5, -10, 1, false, false);
+        ab_is_subset_of_cd!(-15, -5, -5, 1, false, false);
+        ab_is_subset_of_cd!(-15, -5, -2, 1, false, false);
+        ab_is_subset_of_cd!(-15, -5, 0, 1, false, false);
+
+        ab_is_subset_of_cd!(-15, -5, -20, 0, true, true);
+        ab_is_subset_of_cd!(-15, -5, -15, 0, true, true);
+        ab_is_subset_of_cd!(-15, -5, -20, 4, true, true);
+        ab_is_subset_of_cd!(-15, -5, -15, 4, true, true);
+
+        // the empty set is a subset of any set
+        ab_is_subset_of_cd!(-10, -15, -20, 0, true, true);
+        ab_is_subset_of_cd!(-10, -20, -20, 0, true, true);
+        ab_is_subset_of_cd!(-10, -25, -20, 0, true, true);
+        ab_is_subset_of_cd!(-20, -30, -20, 0, true, true);
+        ab_is_subset_of_cd!(-25, -30, -20, 0, true, true);
+
+        // signs
+        // - - + -
+        ab_is_subset_of_cd!(-20, -10, 1, -1, false, false);
+        ab_is_subset_of_cd!(-20, -10, 1, -15, false, false);
+        ab_is_subset_of_cd!(-20, -10, 1, -10, false, false);
+        ab_is_subset_of_cd!(-20, -10, 1, -20, false, false);
+        ab_is_subset_of_cd!(-20, -10, 1, -25, false, false);
+
+        ab_is_subset_of_cd!(-20, -10, 0, -1, false, false);
+        ab_is_subset_of_cd!(-20, -10, 0, -15, false, false);
+        ab_is_subset_of_cd!(-20, -10, 0, -10, false, false);
+        ab_is_subset_of_cd!(-20, -10, 0, -20, false, false);
+        ab_is_subset_of_cd!(-20, -10, 0, -25, false, false);
+
+        // the empty set is a subset of any set
+        ab_is_subset_of_cd!(-10, -20, 1, -1, true, false);
+        ab_is_subset_of_cd!(-10, -20, 1, -15, true, false);
+        ab_is_subset_of_cd!(-10, -20, 1, -10, true, false);
+        ab_is_subset_of_cd!(-10, -20, 1, -20, true, false);
+        ab_is_subset_of_cd!(-10, -20, 1, -25, true, false);
+        ab_is_subset_of_cd!(-10, -20, 0, -1, true, false);
+        ab_is_subset_of_cd!(-10, -20, 0, -15, true, false);
+        ab_is_subset_of_cd!(-10, -20, 0, -10, true, false);
+        ab_is_subset_of_cd!(-10, -20, 0, -20, true, false);
+        ab_is_subset_of_cd!(-10, -20, 0, -25, true, false);
+
+        // signs
+        // - - + +
+        ab_is_subset_of_cd!(-20, -10, 0, 0, false, false);
+        ab_is_subset_of_cd!(-20, -10, 0, 1, false, false);
+        ab_is_subset_of_cd!(-20, -10, 1, 1, false, false);
+        ab_is_subset_of_cd!(-20, -10, 3, 5, false, false);
+
+        // the empty set is a subset of any set
+        ab_is_subset_of_cd!(-10, -20, 1, 1, true, true);
+        ab_is_subset_of_cd!(-10, -20, 1, 2, true, true);
+        ab_is_subset_of_cd!(-10, -20, 2, 1, true, false);
+
+        // signs
+        // - + - -
+        ab_is_subset_of_cd!(-20, 0, -40, -30, false, false);
+        ab_is_subset_of_cd!(-20, 0, -40, -20, false, false);
+        ab_is_subset_of_cd!(-20, 0, -40, -10, false, false);
+        ab_is_subset_of_cd!(-20, 0, -20, -20, false, false);
+        ab_is_subset_of_cd!(-20, 0, -20, -10, false, false);
+        ab_is_subset_of_cd!(-20, 0, -11, -10, false, false);
+        ab_is_subset_of_cd!(-20, 0, -10, -10, false, false);
+
+        ab_is_subset_of_cd!(-20, 3, -40, -30, false, false);
+        ab_is_subset_of_cd!(-20, 2, -40, -20, false, false);
+        ab_is_subset_of_cd!(-20, 1, -40, -10, false, false);
+        ab_is_subset_of_cd!(-20, 3, -20, -20, false, false);
+        ab_is_subset_of_cd!(-20, 2, -20, -10, false, false);
+        ab_is_subset_of_cd!(-20, 1, -11, -10, false, false);
+        ab_is_subset_of_cd!(-20, 3, -10, -10, false, false);
+
+        ab_is_subset_of_cd!(-20, 3, -10, -12, false, false);
+        ab_is_subset_of_cd!(-20, 0, -10, -20, false, false);
+        ab_is_subset_of_cd!(-20, 2, -10, -25, false, false);
+        ab_is_subset_of_cd!(-20, 0, -20, -25, false, false);
+        ab_is_subset_of_cd!(-20, 1, -21, -25, false, false);
+
+        // signs
+        // - + - +
+        ab_is_subset_of_cd!(-20, 0, -19, 0, false, false);
+        ab_is_subset_of_cd!(-20, 0, -19, 1, false, false);
+        ab_is_subset_of_cd!(-20, 1, -19, 0, false, false);
+        ab_is_subset_of_cd!(-20, 1, -19, 1, false, false);
+
+        ab_is_subset_of_cd!(-20, 1, -20, 0, false, false);
+        ab_is_subset_of_cd!(-20, 2, -20, 1, false, false);
+        ab_is_subset_of_cd!(-20, 1, -21, 0, false, false);
+        ab_is_subset_of_cd!(-20, 2, -22, 1, false, false);
+
+        ab_is_subset_of_cd!(-20, 0, -20, 0, true, false);
+        ab_is_subset_of_cd!(-20, 0, -20, 1, true, true);
+        ab_is_subset_of_cd!(-20, 1, -20, 1, true, false);
+        ab_is_subset_of_cd!(-20, 0, -100, 0, true, true);
+        ab_is_subset_of_cd!(-20, 0, -100, 1, true, true);
+        ab_is_subset_of_cd!(-20, 1, -100, 1, true, true);
+
+        // signs
+        // - + + -
+        ab_is_subset_of_cd!(-20, 10, 10, -20, false, false);
+        ab_is_subset_of_cd!(-20, 10, 10, -22, false, false);
+        ab_is_subset_of_cd!(-20, 10, 10, -18, false, false);
+
+        ab_is_subset_of_cd!(-20, 10, 8, -20, false, false);
+        ab_is_subset_of_cd!(-20, 10, 8, -22, false, false);
+        ab_is_subset_of_cd!(-20, 10, 8, -18, false, false);
+
+        ab_is_subset_of_cd!(-20, 10, 12, -20, false, false);
+        ab_is_subset_of_cd!(-20, 10, 12, -22, false, false);
+        ab_is_subset_of_cd!(-20, 10, 12, -18, false, false);
+
+        // signs
+        // - + + +
+        ab_is_subset_of_cd!(-20, 0, 10, 20, false, false);
+        ab_is_subset_of_cd!(-20, 0, 15, 20, false, false);
+        ab_is_subset_of_cd!(-20, 0, 0, 20, false, false);
+        ab_is_subset_of_cd!(-20, 10, 10, 20, false, false);
+        ab_is_subset_of_cd!(-20, 10, 15, 20, false, false);
+        ab_is_subset_of_cd!(-20, 10, 0, 20, false, false);
+
+        // signs
+        // + - - -
+        // the empty set is a subset of any set
+        ab_is_subset_of_cd!(10, -20, -10, -10, true, true);
+        ab_is_subset_of_cd!(0, -20, -20, -10, true, true);
+        ab_is_subset_of_cd!(10, -20, -20, -20, true, true);
+        ab_is_subset_of_cd!(0, -20, -30, -20, true, true);
+        ab_is_subset_of_cd!(10, -20, -30, -30, true, true);
+
+        // signs
+        // + - - +
+        // the empty set is a subset of any set
+        ab_is_subset_of_cd!(10, -20, -18, 10, true, true);
+        ab_is_subset_of_cd!(10, -20, -20, 10, true, true);
+        ab_is_subset_of_cd!(10, -20, -22, 10, true, true);
+
+        ab_is_subset_of_cd!(10, -20, -18, 12, true, true);
+        ab_is_subset_of_cd!(10, -20, -20, 12, true, true);
+        ab_is_subset_of_cd!(10, -20, -22, 12, true, true);
+
+        ab_is_subset_of_cd!(10, -20, -18, 8, true, true);
+        ab_is_subset_of_cd!(10, -20, -20, 8, true, true);
+        ab_is_subset_of_cd!(10, -20, -22, 8, true, true);
+
+        // signs
+        // + - + -
+        // the empty set is a subset of any set
+        ab_is_subset_of_cd!(10, -20, 10, -18, true, false);
+        ab_is_subset_of_cd!(10, -20, 10, -20, true, false);
+        ab_is_subset_of_cd!(10, -20, 10, -22, true, false);
+
+        ab_is_subset_of_cd!(10, -20, 12, -18, true, false);
+        ab_is_subset_of_cd!(10, -20, 12, -20, true, false);
+        ab_is_subset_of_cd!(10, -20, 12, -22, true, false);
+
+        ab_is_subset_of_cd!(10, -20, 8, -18, true, false);
+        ab_is_subset_of_cd!(10, -20, 8, -20, true, false);
+        ab_is_subset_of_cd!(10, -20, 8, -22, true, false);
+
+        // signs
+        // + - + +
+        // the empty set is a subset of any set
+        ab_is_subset_of_cd!(10, -20, 0, 8, true, true);
+        ab_is_subset_of_cd!(10, -20, 0, 10, true, true);
+        ab_is_subset_of_cd!(10, -20, 0, 12, true, true);
+        ab_is_subset_of_cd!(10, -20, 4, 8, true, true);
+        ab_is_subset_of_cd!(10, -20, 4, 10, true, true);
+        ab_is_subset_of_cd!(10, -20, 4, 12, true, true);
+
+        // signs
+        // + + - -
+        ab_is_subset_of_cd!(0, 20, -10, -20, false, false);
+        ab_is_subset_of_cd!(10, 20, -10, -20, false, false);
+
+        // the empty set is a subset of any set
+        ab_is_subset_of_cd!(20, 10, -20, -10, true, true);
+        ab_is_subset_of_cd!(20, 10, -10, -20, true, false);
+
+        // signs
+        // + + - +
+        ab_is_subset_of_cd!(10, 20, -10, 0, false, false);
+        ab_is_subset_of_cd!(10, 20, -10, 5, false, false);
+        ab_is_subset_of_cd!(10, 20, -10, 10, false, false);
+        ab_is_subset_of_cd!(0, 20, -10, 0, false, false);
+        ab_is_subset_of_cd!(0, 20, -10, 5, false, false);
+        ab_is_subset_of_cd!(0, 20, -10, 10, false, false);
+
+        ab_is_subset_of_cd!(10, 20, -10, 20, true, true);
+        ab_is_subset_of_cd!(10, 20, -10, 25, true, true);
+        ab_is_subset_of_cd!(0, 20, -10, 20, true, true);
+        ab_is_subset_of_cd!(0, 20, -10, 25, true, true);
+
+        // the empty set is a subset of any set
+        ab_is_subset_of_cd!(20, 10, -10, 0, true, true);
+        ab_is_subset_of_cd!(20, 10, -10, 25, true, true);
+
+        // signs
+        // + + + -
+        ab_is_subset_of_cd!(10, 20, 0, -10, false, false);
+        ab_is_subset_of_cd!(10, 20, 5, -10, false, false);
+        ab_is_subset_of_cd!(10, 20, 10, -10, false, false);
+        ab_is_subset_of_cd!(10, 20, 20, -10, false, false);
+        ab_is_subset_of_cd!(10, 20, 25, -10, false, false);
+
+        ab_is_subset_of_cd!(0, 20, 0, -10, false, false);
+        ab_is_subset_of_cd!(0, 20, 5, -10, false, false);
+        ab_is_subset_of_cd!(0, 20, 10, -10, false, false);
+        ab_is_subset_of_cd!(0, 20, 20, -10, false, false);
+        ab_is_subset_of_cd!(0, 20, 25, -10, false, false);
+
+        // signs
+        // + + + +
+        test_nonnegative_abcd!(0, 10, 0, 0, false, false);
+        test_nonnegative_abcd!(0, 10, 0, 5, false, false);
+        test_nonnegative_abcd!(0, 10, 0, 10, true, false);
+        test_nonnegative_abcd!(0, 10, 0, 15, true, true);
+
+        test_nonnegative_abcd!(0, 10, 5, 5, false, false);
+        test_nonnegative_abcd!(0, 10, 5, 10, false, false);
+        test_nonnegative_abcd!(0, 10, 5, 15, false, false);
+
+        test_nonnegative_abcd!(0, 10, 10, 10, false, false);
+        test_nonnegative_abcd!(0, 10, 10, 11, false, false);
+
+        test_nonnegative_abcd!(0, 10, 15, 15, false, false);
+        test_nonnegative_abcd!(0, 10, 15, 20, false, false);
+
+        test_nonnegative_abcd!(5, 10, 0, 0, false, false);
+        test_nonnegative_abcd!(5, 10, 0, 2, false, false);
+        test_nonnegative_abcd!(5, 10, 0, 5, false, false);
+        test_nonnegative_abcd!(5, 10, 0, 7, false, false);
+        test_nonnegative_abcd!(5, 10, 0, 10, true, true);
+        test_nonnegative_abcd!(5, 10, 0, 15, true, true);
+
+        test_nonnegative_abcd!(5, 10, 2, 2, false, false);
+        test_nonnegative_abcd!(5, 10, 2, 5, false, false);
+        test_nonnegative_abcd!(5, 10, 2, 7, false, false);
+        test_nonnegative_abcd!(5, 10, 2, 10, true, true);
+        test_nonnegative_abcd!(5, 10, 2, 15, true, true);
+
+        test_nonnegative_abcd!(5, 10, 5, 5, false, false);
+        test_nonnegative_abcd!(5, 10, 5, 7, false, false);
+        test_nonnegative_abcd!(5, 10, 5, 10, true, false);
+        test_nonnegative_abcd!(5, 10, 5, 15, true, true);
+
+        test_nonnegative_abcd!(5, 10, 8, 8, false, false);
+        test_nonnegative_abcd!(5, 10, 8, 9, false, false);
+        test_nonnegative_abcd!(5, 10, 8, 10, false, false);
+        test_nonnegative_abcd!(5, 10, 8, 12, false, false);
+
+        test_nonnegative_abcd!(5, 10, 10, 10, false, false);
+        test_nonnegative_abcd!(5, 10, 10, 15, false, false);
+
+        test_nonnegative_abcd!(5, 10, 15, 15, false, false);
+        test_nonnegative_abcd!(5, 10, 15, 16, false, false);
+
+        // the empty set is a subset of any set
+        test_nonnegative_abcd!(10, 0, 0, 8, true, true);
+        test_nonnegative_abcd!(10, 0, 0, 10, true, true);
+        test_nonnegative_abcd!(10, 0, 0, 12, true, true);
+        test_nonnegative_abcd!(10, 0, 1, 8, true, true);
+        test_nonnegative_abcd!(10, 0, 1, 10, true, true);
+        test_nonnegative_abcd!(10, 0, 1, 12, true, true);
+        test_nonnegative_abcd!(10, 2, 0, 8, true, true);
+        test_nonnegative_abcd!(10, 2, 0, 10, true, true);
+        test_nonnegative_abcd!(10, 2, 0, 12, true, true);
+        test_nonnegative_abcd!(10, 2, 1, 8, true, true);
+        test_nonnegative_abcd!(10, 2, 1, 10, true, true);
+        test_nonnegative_abcd!(10, 2, 1, 12, true, true);
+
+        test_nonnegative_abcd!(10, 0, 10, 0, true, false);
+        test_nonnegative_abcd!(10, 0, 10, 2, true, false);
+        test_nonnegative_abcd!(10, 2, 10, 0, true, false);
+        test_nonnegative_abcd!(10, 2, 10, 2, true, false);
+        test_nonnegative_abcd!(10, 2, 10, 4, true, false);
     }
 }
